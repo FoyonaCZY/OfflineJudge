10c0ebbb14bd251921c5d058904154ec9d17b230
FoyonaCZY <30814347+FoyonaCZY@users.noreply.github.com>
2024-09-07 16:30:09 +0800

    Add files via upload

--- utils/utils.go
+++ utils/utils.go
@@ -8,6 +8,12 @@ import (
 var SaveData func() = nil
 
 func Response(c *gin.Context, data interface{}) {
+	o := c.Request.Header.Get("Origin")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Origin", o)
+	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
 		"msg":  "R U OK?",
