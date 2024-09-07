1b0fec6e69bfafcf404fa9da74c6766e10c29e97
FoyonaCZY <30814347+FoyonaCZY@users.noreply.github.com>
2024-09-07 16:28:47 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 )
 
 func main() {
-	utils.SaveData = io.Update
 
 	r := gin.Default()
 
@@ -19,7 +18,18 @@ func main() {
 	submission.Init(r)
 	status.Init(r)
 	contest.Init(r)
+	utils.SaveData = io.Update
 	io.Init()
+
+	r.OPTIONS("/*path", func(c *gin.Context) {
+		o := c.Request.Header.Get("Origin")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", o)
+		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Content-Type", "application/json")
+		c.String(200, "")
+	})
 
 	err := r.Run(":12138")
 	if err != nil {
